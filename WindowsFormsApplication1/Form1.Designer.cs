67d7992ed1cfb7223aeea1c0f2384e6238314a08
Jonathan Benitez <Jonathan Benitez>
2016-09-25 19:13:24 -0700

    firs tictactoeDummy

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 59);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(349, 330);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gato";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 401);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+       
+        Button[] btn = new Button[9];
+
+        private void WhoWin()
+        {
+            //[0][1][2]
+            //[3][4][5]
+            //[6][7][8]
+            if((btn[0].Text=="X" && btn[1].Text == "X" && btn[2].Text == "X") ||
+                (btn[3].Text == "X" && btn[4].Text == "X" && btn[5].Text == "X")||
+                (btn[6].Text == "X" && btn[7].Text == "X" && btn[8].Text == "X")||
+                (btn[0].Text == "X" && btn[3].Text == "X" && btn[6].Text == "X") ||
+                (btn[1].Text == "X" && btn[4].Text == "X" && btn[7].Text == "X") ||
+                (btn[2].Text == "X" && btn[5].Text == "X" && btn[8].Text == "X") ||
+                (btn[0].Text == "X" && btn[4].Text == "X" && btn[8].Text == "X") ||
+                (btn[6].Text == "X" && btn[4].Text == "X" && btn[2].Text == "X") )
+            {
+                MessageBox.Show("Player wins");
+                reset();
+            }else if((btn[0].Text == "O" && btn[1].Text == "O" && btn[2].Text == "O") ||
+                (btn[3].Text == "O" && btn[4].Text == "O" && btn[5].Text == "O") ||
+                (btn[6].Text == "O" && btn[7].Text == "O" && btn[8].Text == "O") ||
+                (btn[0].Text == "O" && btn[3].Text == "O" && btn[6].Text == "O") ||
+                (btn[1].Text == "O" && btn[4].Text == "O" && btn[7].Text == "O") ||
+                (btn[2].Text == "O" && btn[5].Text == "O" && btn[8].Text == "O") ||
+                (btn[0].Text == "O" && btn[4].Text == "O" && btn[8].Text == "O") ||
+                (btn[6].Text == "O" && btn[4].Text == "O" && btn[2].Text == "O")){
+                MessageBox.Show("Maquina wins");
+                reset();
+            }
+        }
+
+        private void Machine()
+        {
+
+            for (int i = 0; i <= 1; i++)
+            {
+                Random random = new Random();
+                int numero = random.Next(1, 9);
+                if (btn[numero].Text == "")
+                {
+                    btn[numero].Text = "O";
+                    btn[numero].Enabled = false;
+                    count++;
+                    i = 1;
+                }else
+                {
+                    i = 0;
+                }
+
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                btn[i] = new Button();
+                btn[i].Width = 100;
+                btn[i].Height = 100;
+                btn[i].Click += new EventHandler(Form1_Click);
+                flowLayoutPanel1.Controls.Add(btn[i]);
+            }
+        }
+        void reset()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                btn[i].Enabled = true;
+                btn[i].Text = "";
+                count = 0;
+            }
+        }
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            
+            Button boton = (Button)sender;
+            count++;
+            if (count == 9)
+            {
+                boton.Text = "X";
+             
+                WhoWin();
+               
+                MessageBox.Show("Game Draw");
+                reset();
+                
+            }else if(count < 9)
+            {  
+                boton.Text = "X";
+                boton.Enabled = false;
+                Machine();
+                WhoWin();
+                
+            }
+            
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
